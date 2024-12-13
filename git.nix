91aa94f7cc0f3e9111ac3f62a73fb08a2e047308
flowerhill <tomoki19880910@gmail.com>
2024-12-13 22:58:08 +0900

    add: git.nix

---
+++ git.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+  programs.git = {
+    enable = true;
+    userName = "flowerhill";
+    userEmail = "tomoki19880910@gmail.com";
+  };
+
+  programs.gh = {
+    enable = true;
+    extensions = with pkgs; [gh-markdown-preview];
+    settings = {
+      editor = "nvim";
+    };
+  };
+}
